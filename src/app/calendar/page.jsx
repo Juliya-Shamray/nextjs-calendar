a05e196c7a213d3990c5fc751cea229be5f56d90
Juliya Shamray <fizmatka908990@gmail.com>
2024-01-26 21:30:22 +0200

    calendar

--- src/app/calendar/page.jsx
+++ src/app/calendar/page.jsx
@@ -10,7 +10,7 @@ import { fetchEvents } from "@/redux/calendar/operations";
 const minutesToTime = (minutes) => {
   const hours =
     Math.floor(minutes / 60) + 8 >= 13
-      ? Math.floor(Math.floor(minutes / 60 + 8) / 12)
+      ? Math.floor(Math.floor(minutes / 60 + 8) - 12)
       : Math.floor(minutes / 60) + 8;
   const mins = minutes % 60;
   return `${String(hours)}:${String(mins).padStart(2, "0")}`;
@@ -31,28 +31,9 @@ const generateTimeSlots = () => {
     timeSlots.push({
       time: `${hour}:${minute === 0 ? "00" : minute}`,
       isHalf: i % 2 === 1,
-      isVisible: true,
     });
-
-    // Додавання додаткових слотів між 8:00 та 9:00
-    if (i < 18 && i % 2 === 0) {
-      const extraMinute1 = minute + 10;
-      const extraMinute2 = minute + 20;
-
-      timeSlots.push({
-        time: `${hour}:${extraMinute1 === 0 ? "00" : extraMinute1}`,
-        isHalf: true,
-        isVisible: false,
-      });
-
-      timeSlots.push({
-        time: `${hour}:${extraMinute2 === 0 ? "00" : extraMinute2}`,
-        isHalf: true,
-        isVisible: false,
-      });
-    }
   }
-  console.log(timeSlots.length);
+  console.log(timeSlots);
   return timeSlots;
 };
 
@@ -95,15 +76,7 @@ const MyCalendar = () => {
 
   const calculateEventHeight = (event) => {
     const slotDuration = 15;
-
     return (event.duration / slotDuration) * 30;
-  };
-
-  const calculateEventWidth = (eventsForSlot) => {
-    if (eventsForSlot.length > 1) {
-      return "50%";
-    }
-    return "100%";
   };
 
   const findEventsForSlot = (slot) => {
@@ -112,16 +85,13 @@ const MyCalendar = () => {
     );
 
     if (eventsForSlot.length > 0) {
-      const eventStartTimes = eventsForSlot.map((event) =>
-        parseTimeToMinutes(minutesToTime(event.start))
-      );
       const slotStartTime = parseTimeToMinutes(slot.time);
 
-      const TIME_TOLERANCE = 20;
-      return eventsForSlot.filter(
-        (event, index) =>
-          Math.abs(eventStartTimes[index] - slotStartTime) <= TIME_TOLERANCE
-      );
+      return eventsForSlot.map((event) => {
+        const eventStartTime = parseTimeToMinutes(minutesToTime(event.start));
+        const slotIndex = Math.floor((eventStartTime - slotStartTime) / 30);
+        return { ...event, slotIndex };
+      });
     }
 
     return [];
@@ -130,71 +100,85 @@ const MyCalendar = () => {
   return (
     <PrivateRoutes>
       <div className="container">
+        <button onClick={() => openModal()}>Add event</button>
         <div className="wrapper">
           <ul>
-            {timeSlots.slice(0, 19).map((slot, index) => {
+            {timeSlots.slice(0, 10).map((slot, index) => {
               const eventsForSlot = findEventsForSlot(slot);
-              console.log(eventsForSlot);
-
               return (
                 <li
                   key={index}
                   className={`${
                     slot.isHalf ? "time half-hour" : "time full-hour"
-                  } ${slot.isVisible ? "visible" : "invisible"}`}
-                  onClick={() => openModal(slot)}
+                  } `}
                 >
-                  <span>{slot.time}</span>
+                  <div className="slot">{slot.time}</div>
                   <div className="events">
                     {eventsForSlot.length > 0 &&
-                      eventsForSlot.map((event, eventIndex) => (
-                        <div
-                          key={eventIndex}
-                          className="event"
-                          style={{
-                            height: calculateEventHeight(event) + "px",
-                            width: calculateEventWidth(eventsForSlot),
-                          }}
-                        >
-                          {`${event.title}`}
-                        </div>
-                      ))}
+                      eventsForSlot.map((event, eventIndex) => {
+                        const eventStartTime = parseTimeToMinutes(
+                          minutesToTime(event.start)
+                        );
+                        const slotStartTime = parseTimeToMinutes(slot.time);
+                        const timeDifference = eventStartTime - slotStartTime;
+                        return (
+                          <div
+                            key={eventIndex}
+                            className="event"
+                            style={{
+                              height: calculateEventHeight(event, slot) + "px",
+                              visibility:
+                                event.slotIndex === 0 ? "visible" : "hidden",
+                              marginTop: `${2 * timeDifference}px`,
+                            }}
+                          >
+                            {event.slotIndex === 0 && `${event.title}`}
+                          </div>
+                        );
+                      })}
                   </div>
                 </li>
               );
             })}
           </ul>
           <ul>
-            {timeSlots.slice(19).map((slot, index) => {
+            {timeSlots.slice(10).map((slot, index) => {
               const eventsForSlot = findEventsForSlot(slot);
-              console.log(eventsForSlot);
-              if (index % 2 === 0)
-                return (
-                  <li
-                    key={index}
-                    className={`${
-                      slot.isHalf ? "time half-hour" : "time full-hour"
-                    } ${slot.isVisible ? "visible" : "invisible"}`}
-                    onClick={() => openModal(slot)}
-                  >
-                    <span>{slot.time}</span>
-                    <div className="events">
-                      {eventsForSlot.length > 0 &&
-                        eventsForSlot.map((event, eventIndex) => (
+
+              return (
+                <li
+                  key={index}
+                  className={`${
+                    slot.isHalf ? "time half-hour" : "time full-hour"
+                  } `}
+                >
+                  <div className="slot">{slot.time}</div>
+                  <div className="events">
+                    {eventsForSlot.length > 0 &&
+                      eventsForSlot.map((event, eventIndex) => {
+                        const eventStartTime = parseTimeToMinutes(
+                          minutesToTime(event.start)
+                        );
+                        const slotStartTime = parseTimeToMinutes(slot.time);
+                        const timeDifference = eventStartTime - slotStartTime;
+                        return (
                           <div
                             key={eventIndex}
                             className="event"
                             style={{
-                              height: calculateEventHeight(event) + "px",
-                              width: calculateEventWidth(eventsForSlot),
+                              height: calculateEventHeight(event, slot) + "px",
+                              visibility:
+                                event.slotIndex === 0 ? "visible" : "hidden",
+                              marginTop: `${2 * timeDifference}px`,
                             }}
                           >
-                            {`${event.title}`}
+                            {event.slotIndex === 0 && `${event.title}`}
                           </div>
-                        ))}
-                    </div>
-                  </li>
-                );
+                        );
+                      })}
+                  </div>
+                </li>
+              );
             })}
           </ul>
         </div>
